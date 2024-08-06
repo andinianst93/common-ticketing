c1a1e1e1b25f542a16436af6ec84c5626168e3e1
andinianst93 <andinidev93@gmail.com>
2024-08-06 17:09:21 +0700

    add order events

--- src/events/order-created-event.ts
+++ src/events/order-created-event.ts
@@ -5,7 +5,6 @@ export interface OrderCreatedEvent {
   subject: Subjects.OrderCreated;
   data: {
     id: string;
-    version: number;
     status: OrderStatus;
     userId: string;
     expiresAt: string;
